2f15c57e6f874b61e32cd6e3d819aae9b4ccc204
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-06-21 11:03:29 +0700

    mumbuka file dalam mode membaca trus menunjukke sebuah file dan dengan kondisi jika file ada maka melakukan perulangan setiap baris dan menampilkan kemudian tutuup file tersebut setelah selesai. jika tidak ditemukan file maka akan menampilkan pesan unable to open file

--- pustaka file & exception handling/pustaka file & exception handling.cpp
+++ pustaka file & exception handling/pustaka file & exception handling.cpp
@@ -28,5 +28,28 @@ int main() {
 	//selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
-	
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidaak ditemukan file maka akan menampilkan pesan ini
+	else cout << "Unable to open file";
+
+	return 0;
+}
+
 	
